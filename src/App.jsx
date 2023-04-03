cd970dba6755da483cf6a15e914769ee9926f714
dnozaki <dnozaki88888888@gmail.com>
2023-04-03 15:42:48 +0900

    implement save note to localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import './App.css'
 import Main from "./components/Main"
 import Sidebar from "./components/Sidebar"
 import uuid from "react-uuid"
+import { useEffect } from 'react'
 
 function App() {
-  const [notes, setNotes ] = useState([])
+  const [notes, setNotes ] = useState(JSON.parse(localStorage.getItem("notes")) || [])
   const [activeNote, setActiveNote] = useState(false)
+
+  useEffect (() => {
+    //ローカルストレージにノートを保存する
+    localStorage.setItem("notes", JSON.stringify(notes))
+  }, [notes])
 
   const onAddNote = () => {
     console.log("新しくノートが追加されました。")
